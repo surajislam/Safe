d8f14c6ebdd45b5ff9e0517ad9e73d158fcc64d5
surajislam <154444093+surajislam@users.noreply.github.com>
2025-11-28 21:19:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,1 @@
-import re
-import os
-from os import getenv
-
-from dotenv import load_dotenv
-from pyrogram import filters
-
-load_dotenv()
-
-# Get your token from @BotFather on Telegram.
-TELEGRAM_BOT_TOKEN = getenv("TELEGRAM_BOT_TOKEN", "8578397940:AAHj6zglBBc6DJtSzH4kLQfv0uu-KtbyK1A")
+#
